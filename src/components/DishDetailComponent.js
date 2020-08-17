57cda9be4c9e8d71cea4d4aec6ff8e646579168f
sriram <sriram1999m@gmail.com>
2020-08-17 15:17:08 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const DishDetail = (props) => {
     if (props.isLoading) {
         return (
@@ -67,24 +68,28 @@ function RenderComments({ comments, postComment, dishId }) {
         }
         const cmnts = comments.map(comment => {
             return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
-                    </p>
-                </li>
+               <Fade in>
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author},
+                            &nbsp;
+                            {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: '2-digit'
+                        }).format(new Date(comment.date))}
+                        </p>
+                    </li>
+               </Fade>
             )
         })
         return (
             <div >
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
-                    {cmnts}
+                    <Stagger in>
+                        {cmnts}
+                    </Stagger>
                 </ul>
                
                 <CommentForm dishId={dishId} postComment={postComment} />
@@ -95,13 +100,15 @@ function RenderComments({ comments, postComment, dishId }) {
         if (dish != null)
             return (
                 <div>
-                    <Card>
-                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                   <FadeTransform in transfomProps={{exitTransform:"scale(0.5) translateY(-50%)"}}>
+                        <Card>
+                            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                   </FadeTransform>
                 </div>
             );
         else
